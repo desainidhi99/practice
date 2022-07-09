df2b81299a74d935e6d7e0509a30a039efd8de7f
Desai <nidhi.desai@almacgroup.com>
2022-07-09 14:50:21 -0400

    first commit - two sum

---
+++ TwoSum.java
@@ -0,0 +1,28 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        if(nums.length ==0)
+        {
+            return new int[]{};
+        }
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++)
+        {
+            map.put(nums[i], i);
+        }
+
+        for(int i=0; i<nums.length; i++)
+        {
+            int diff = target - nums[i];
+            if(map.containsKey(diff) && i != map.get(diff))
+            {
+                return  new int[]{i, map.get(diff)};
+            }
+
+        }
+
+        return new int[]{};
+    }
+}
