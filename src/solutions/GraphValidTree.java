8c52684750269e387a2bacc566e63fc71f9d8a1e
desainidhi99 <62072571+desainidhi99@users.noreply.github.com>
2022-07-27 23:47:20 -0400

    adding climbing stairs

---
+++ src/solutions/GraphValidTree.java
@@ -0,0 +1,10 @@
+/*
+You have a graph of n nodes labeled from 0 to n - 1. You are given an integer n and a list of edges where edges[i] = [ai, bi] indicates that there is an undirected edge between nodes ai and bi in the graph.
+
+Return true if the edges of the given graph make up a valid tree, and false otherwise.
+ */
+
+/*
+Input: n = 5, edges = [[0,1],[0,2],[0,3],[1,4]]
+Output: true
+ */
