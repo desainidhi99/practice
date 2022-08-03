b45878d8af16faff530b6d87a1405765cb20e2d3
desainidhi99 <62072571+desainidhi99@users.noreply.github.com>
2022-08-03 00:12:30 -0400

    adding containsduplicatesecondpart

--- src/solutions/ContainsDuplicateSecondPart.java
+++ src/solutions/ContainsDuplicateSecondPart.java
@@ -32,5 +32,25 @@ Output: false
 
  */
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicateSecondPart {
+    public boolean containsDuplicate(int[] nums) {
+
+        if(nums == null || nums.length == 0)
+            return false;
+
+        Set<Integer> set = new HashSet<Integer>();
+
+
+        for(int i = 0; i < nums.length; i++){
+            if (set.contains(nums[i]))
+                return true;
+            set.add(nums[i]);
+        }
+
+
+        return false;
+    }
 }
